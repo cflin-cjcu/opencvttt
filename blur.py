c34a4ec4e08a0c34cc97e99f937b32b86a615680
cflin <cflin@mail.cjcu.edu.tw>
2021-07-27 18:08:10 +0800

    morphology練習

--- blur.py
+++ blur.py
@@ -2,13 +2,19 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
+kernel = np.ones((5, 5), np.uint8)
 img = cv.imread('./images/test1.jpg',0)
 ret3, th3 = cv.threshold(img, 0, 255, cv.THRESH_TOZERO+cv.THRESH_OTSU)
-roi = th3[200:400,400:800]
-gauss = cv.GaussianBlur(roi, (11, 11), 3)
-th3[200:400,400:800]=gauss
+erosion = cv.erode(th3, kernel, iterations=1)
+dilation = cv.dilate(th3, kernel, iterations=1)
+gradient = cv.morphologyEx(img, cv.MORPH_GRADIENT, kernel)
+# roi = th3[200:400,400:800]
+# gauss = cv.GaussianBlur(roi, (11, 11), 3)
+# th3[200:400,400:800]=gauss
 cv.imshow('img',img)
 cv.imshow('th3',th3)
-cv.imshow('gauss',gauss)
+cv.imshow('gauss',erosion)
+cv.imshow('dilation',dilation)
+cv.imshow('gradient',gradient)
 cv.waitKey(0)
 cv.destroyAllWindows()
