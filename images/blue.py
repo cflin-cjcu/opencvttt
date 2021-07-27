ff268fcc26743e6b428c8eae122ea18b9c554fd1
cflin <cflin@mail.cjcu.edu.tw>
2021-07-27 11:14:30 +0800

    HSV練習

---
+++ images/blue.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+#開圖
+img1 = cv2.imread('./images/test1.jpg')
+img2 = cv2.cvtColor(img1,cv2.COLOR_BGR2HSV)
+cv2.namedWindow('img1')
+cv2.createTrackbar('A','img1',0,255,nothing)
+cv2.createTrackbar('B','img1',0,255,nothing)
+
+while(True):
+    A = cv2.getTrackbarPos('A','img1')
+    B = cv2.getTrackbarPos('B','img1')
+    lower_blue = np.array([A,50,50])
+    upper_blue = np.array([B,255,255])
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(img2, lower_blue, upper_blue)
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(img1,img1, mask= mask)
+    #秀圖
+    cv2.imshow('img1',img1)
+    cv2.imshow('img2',img2)
+    cv2.imshow('res',res)
+    k= cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+cv2.destroyAllWindows()
--- logo.py
+++ logo.py
@@ -4,8 +4,11 @@ import cv2 as cv
 img1 = cv.imread('./images/test1.jpg')
 img2 = cv.imread('./images/logo.png')
 #我想在左上角放置一个logo，所以我创建了一个 ROI,并且这个ROI的宽和高为我想放置的logo的宽和高
+h, w, s = img1.shape
 rows,cols,channels = img2.shape
 roi = img1[0:rows,0:cols]
+# img1[h-rows:h,0:cols]
+cv.cvtColor(cv.COLOR_BGR2HSV)
 #现在创建一个logo的掩码，通过对logo图像进行阈值，并对阈值结果并创建其反转掩码
 img2gray = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 ret,mask = cv.threshold(img2gray,0,100,cv.THRESH_BINARY)
@@ -16,7 +19,7 @@ img1_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
 img2_fg = cv.bitwise_and(img2,img2,mask = mask)
 #在 ROI 中放置徽标并修改主图像
 dst = cv.add(img1_bg,img2_fg)
-img1 [0:rows,0:cols] = dst
+img1[0:rows,0:cols] = dst
 cv.imshow('img2gray',img2gray)
 cv.imshow('mask',mask)
 cv.imshow('mask_inv',mask_inv)
