865a997701c28c9d34f80b2e9c2f4d7ce0bb362e
cflin <cflin@mail.cjcu.edu.tw>
2021-07-28 10:09:13 +0800

    scharr 修改

--- scharr.py
+++ scharr.py
@@ -3,13 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv.imread('./images/test1.jpg',0)
-scharrx = cv.Scharr(img,cv.CV_8U,1,0)
-scharry = cv.Scharr(img,cv.CV_8U,0,1)
-scharr = cv.Scharr(img,cv.CV_8U,0,1)
+scharrx = cv.Scharr(img,cv.CV_64F,1,0)
+scharry = cv.Scharr(img,cv.CV_64F,0,1)
+scharrx = cv.convertScaleAbs(scharrx)
+scharry = cv.convertScaleAbs(scharry)
+scharrxy = cv.addWeighted(scharrx,0.5,scharry,0.5,0)
 
 cv.imshow('img',img)
 cv.imshow('scharrx',scharrx)
 cv.imshow('scharry',scharry)
-cv.imshow('scharr',scharr)
+cv.imshow('scharrxy',scharrxy)
 cv.waitKey(0)
 cv.destroyAllWindows()
