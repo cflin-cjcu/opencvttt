6eb06eb0b742022ba745daa90d0ef84d922888cd
cflin <cflin@mail.cjcu.edu.tw>
2021-07-29 11:12:48 +0800

    hist練習3

--- hist2.py
+++ hist2.py
@@ -7,7 +7,8 @@ img1 = cv2.imread('./images/test1.jpg', 0)
 hist1 = cv2.calcHist([img1], [0], None, [256], [0, 256])
 
 mask = np.zeros(img1.shape[:2], np.uint8)
-mask[100:600, 400:800] = 255
+mask[:,:]=255
+mask[100:600, 400:800] = 0
 masked_img = cv2.bitwise_and(img1,img1,mask = mask)
 hist_full = cv2.calcHist([masked_img],[0],None,[256],[0,256])
 hist_mask = cv2.calcHist([img1],[0],mask,[256],[0,256])
